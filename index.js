e3142767c2e505fbceb0ae82c14c565ad1b739e7
Fariya-Khan-Web <fariya.webdev@gmail.com>
2025-01-20 05:15:35 +0600

    feat: get & delete task

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleweres
@@ -14,35 +14,58 @@ console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const database = client.db('nanotasks_db')
+        const taskCollection = database.collection('tasks')
+
+        // task related apis
+        app.get('/tasks', async (req, res) => {
+            const result = await taskCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/tasks/:email', async(req, res)=>{
+            const email = req.params.email
+            const query = {buyer_email: email}
+            const result = await taskCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('NanoTasks server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server running on port: ${port}`);
 })
