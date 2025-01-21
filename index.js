540316e7f266e31b8f1a7f0755bbb0311b2c848a
Fariya-Khan-Web <fariya.webdev@gmail.com>
2025-01-22 04:26:13 +0600

    feat: get single task with id

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/task/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
+
         app.post('/tasks', async (req, res) => {
             const task = req.body
             const result = await taskCollection.insertOne(task)
